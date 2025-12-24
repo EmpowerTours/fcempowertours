c83bc2a88b0f4dae277a86267fdc6bdb394ab0fa
EmpowerTours <empowertours@gmail.com>
2025-12-23 21:42:26 -0600

    fix: remove broken pages and components
    - Delete events, concierge, beat-match, country-collector pages
    - Delete DemandSignalDisplay, EventList, StakeTours, TandaGroup, PortfolioDisplay components
    - Clean up component exports

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,9 +1,4 @@
 // Export all components
 export { MintPassport } from './MintPassport';
-export { StakeTours } from './StakeTours';
-export { PortfolioDisplay } from './PortfolioDisplay';
 export { PassportStamps } from './PassportStamps';
-export { DemandSignalDisplay } from './DemandSignalDisplay';
-export { EventList } from './EventList';
-export { TandaGroup } from './TandaGroup';
 export { CreditScoreBadge } from './CreditScoreBadge';
